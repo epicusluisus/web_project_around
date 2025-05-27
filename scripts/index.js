8288fd7e4ea5b82243c1e5b2293af5c48b44e5c2
epicusluisus <elquionez23@gmail.com>
2025-05-26 22:29:58 -0700

    fxed img alts and closed divs

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,10 @@ const header = page.querySelector('.header');
 
 const addButtonName = header.querySelector('.header__profile-button');
 const closeButtonName = popup.querySelector('.popup__close-button');
-const profileName = header.querySelector('header__profile-name');
-const profileJob = header.querySelector('header__profile-tag');
-const nameInput = popup.querySelector('#input-name');
-const jobInput = popup.querySelector('#input-tag');
+let profileName = header.querySelector('.header__profile-name');
+let profileJob = header.querySelector('.header__profile-tag');
+let nameInput = popup.querySelector('#input-name');
+let jobInput = popup.querySelector('#input-tag');
 const form = popup.querySelector('.popup__form');
 
 function openPopup() {
@@ -23,14 +23,18 @@ function closePopup() {
 addButtonName.addEventListener('click', openPopup);
 closeButtonName.addEventListener('click', closePopup);
 
-form.addEventListener('submit', handleProfileFormSubmit);
 
 //form n bits
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
 
+form.addEventListener('submit', handleProfileFormSubmit);
+
+/*ayudeme porfavor, no le entiendo a esto, no le enuentro solución
+terminé el diseño de toda la página en 3 diás y llevo 4 en el java
+repasé todo el sprint 7 dos veces
+Ayuda
