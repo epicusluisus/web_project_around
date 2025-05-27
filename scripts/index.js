fa56beed5e2dd9435f015019a9be12c84e2fb310
epicusluisus <elquionez23@gmail.com>
2025-05-26 21:49:16 -0700

    improved code

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
-//pop up n bits
 const page = document.querySelector('.page');
 const popup = page.querySelector('.popup');
 const header = page.querySelector('.header');
 
 const addButtonName = header.querySelector('.header__profile-button');
 const closeButtonName = popup.querySelector('.popup__close-button');
-const saveButtonName = popup.querySelector('.popup__save-button');
-let profileName = header.querySelector('header__profile-name');
-let profileJob = header.querySelector('header__profile-tag');
-let nameInput = popup.querySelector('.popup__form-element_name');
-let jobInput = popup.querySelector('.popup__form-element_tag');
-let form = popup.querySelector('.popup__form');
+const profileName = header.querySelector('header__profile-name');
+const profileJob = header.querySelector('header__profile-tag');
+const nameInput = popup.querySelector('#input-name');
+const jobInput = popup.querySelector('#input-tag');
+const form = popup.querySelector('.popup__form');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -25,17 +23,14 @@ function closePopup() {
 addButtonName.addEventListener('click', openPopup);
 closeButtonName.addEventListener('click', closePopup);
 
-
+form.addEventListener('submit', handleProfileFormSubmit);
 
 //form n bits
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  console.log(nameInput.value);
   closePopup();
 }
 
-form.addEventListener('submit', handleProfileFormSubmit);
