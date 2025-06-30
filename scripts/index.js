9c5d6be7a7679c26a2f57382f491ce3de2d435b8
epicusluisus <elquionez23@gmail.com>
2025-06-29 19:33:10 -0700

    feat: added trash bin and started addCard function

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
-let editButton = document.querySelector('.header__profile-button');
-let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__save-button');
-let popup = document.querySelector('.popup');
+let editButton = document.querySelector(".header__profile-button");
+let closeButton = document.querySelector(".popup__close-button");
+let saveButton = document.querySelector(".popup__save-button");
+let popup = document.querySelector(".popup");
 
-let nameInput = document.querySelector('#input-name');
-let jobInput = document.querySelector('#input-tag');
-let name = document.querySelector('.header__profile-name');
-let job = document.querySelector('.header__profile-tag');
-let form = document.querySelector('.popup__container');
+let nameInput = document.querySelector("#input-name");
+let jobInput = document.querySelector("#input-tag");
+let name = document.querySelector(".header__profile-name");
+let job = document.querySelector(".header__profile-tag");
+let form = document.querySelector(".popup__container");
 
 function openpopup() {
-  popup.classList.add('popup_opened');
+  popup.classList.add("popup_opened");
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
 }
 
 function closepopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener('click' , openpopup);
-closeButton.addEventListener('click' , closepopup);
+editButton.addEventListener("click", openpopup);
+closeButton.addEventListener("click", closepopup);
 
 function submitForm(e) {
   e.preventDefault();
@@ -29,4 +29,27 @@ function submitForm(e) {
   closepopup();
 }
 
-form.addEventListener('submit' , submitForm );
+form.addEventListener("submit", submitForm);
+
+cardLike = document
+  .querySelector(".card__like")
+  .addEventListener("click", function (evt) {
+    evt.target.classList.toggle("card__like_active");
+    evt.target.classList.toggle("button");
+    console.log("hello there");
+  });
+
+function addSong(artistValue, titleValue) {
+  const songTemplate = document.querySelector("#song-template").content;
+  const songElement = songTemplate.querySelector(".song").cloneNode(true);
+
+  songElement.querySelector(".song__artist").textContent = artistValue;
+  songElement.querySelector(".song__title").textContent = titleValue;
+  songElement
+    .querySelector(".song__like")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("song__like_active");
+    });
+
+  songsContainer.append(songElement);
+}
