1e0e965da5223e35eb0a80fc2012b36abfc34fd2
epicusluisus <elquinonez23@gmail.com>
2025-12-03 07:37:42 -0700

    added arrow func

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,8 @@ const magicButton = document.querySelector(".page");
 const contentGrid = document.querySelector(".content__grid");
 const noPostText = document.querySelector(".content__no-post");
 
-function noPost() {
-  noPostText.classList.remove("content__no-post__hidden");
-}
+const noPost = () => noPostText.classList.remove("content__no-post__hidden");
+const hideNoPost = () => noPostText.classList.add("content__no-post__hidden");
 
 function addCard(cardTitle, cardLink) {
   const cardTemplate = document.querySelector("#card-template").content;
@@ -14,25 +13,33 @@ function addCard(cardTitle, cardLink) {
   card.querySelector(".card__image").src = cardLink;
   card.querySelector(".card__image").alt = `Photo of ${cardTitle}`;
 
-  card.querySelector(".card__like").addEventListener("click", function (e) {
+  card.querySelector(".card__like").addEventListener("click", (e) => {
     e.target.classList.toggle("card__like_active");
   })
-  card.querySelector(".card__delete-button").addEventListener("click", function (e) {
+  card.querySelector(".card__delete-button").addEventListener("click", (e) => {
     e.target.parentElement.remove();
     if (contentGrid.children.length == 0) {
-      console.log("bro");
       noPost();
     }
   })
-  contentGrid.append(card);
+  contentGrid.prepend(card);
 
-  card.querySelector(".card__image").addEventListener("click", function(e) {
+  card.querySelector(".card__image").addEventListener("click", (e) => {
     console.log(e.target.alt, e.target.src);
     renderBigImage(e.target.alt, e.target.src);
   })
+  contentGrid.children.length >= 1 ? hideNoPost() : console.log("dale");
 }
 
 const initialCards = [
+  {
+    "title": "Seattle, Washington",
+    "link": "https://images.unsplash.com/photo-1762838896833-ffb8f3032374?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    "title": "Hojas de otoño",
+    "link": "https://images.unsplash.com/photo-1762776345918-dbc968a5fcb0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
   {
     "title": "Valle de Yosemite",
     "link": "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
@@ -48,18 +55,10 @@ const initialCards = [
   {
     "title": "Vanoise National Park",
     "link": "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
-  },
-  {
-    "title": "Seattle, Washington",
-    "link": "https://images.unsplash.com/photo-1762838896833-ffb8f3032374?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  },
-  {
-    "title": "Hojas de otoño",
-    "link": "https://images.unsplash.com/photo-1762776345918-dbc968a5fcb0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
-function addInitialCards() {
+const addInitialCards = () => {
   initialCards.forEach((item) => {
     addCard(item.title, item.link);
   })
@@ -100,7 +99,6 @@ function openForm(title, ph1, ph2) {
   function closeForm() {
     popup.remove();
     popupForm.remove();
-    e.preventDefault();
   }
   popupForm.querySelector(".popup__close-button").addEventListener("click", function (e) {
     closeForm();
@@ -110,8 +108,6 @@ function openForm(title, ph1, ph2) {
 
 //the shitshow
 //begins...
-//experimental things
-
 const editButton = document.querySelector(".header__edit-button");
 const addButton = document.querySelector(".header__add-button");
 const closeButton = document.querySelector(".popup__close-button");
@@ -120,17 +116,11 @@ const saveButton = document.querySelector(".pop__save-button");
 let userName = document.querySelector(".header__profile-name");
 let userTag = document.querySelector(".header__profile-tag");
 
+const editProfile = () => openForm("Editar perfil", "Nombre", "Acerca de mi");
+const addPhoto = () => openForm("Nuevo Lugar", "Título", "Enlace a la imagen");
+
 editButton.addEventListener("click", editProfile);
 addButton.addEventListener("click", addPhoto);
-
-function editProfile() {
-  openForm("Editar perfil", "Nombre", "Acerca de mi");
-}
-
-function addPhoto() {
-  openForm("Nuevo Lugar", "Título", "Enlace a la imagen");
-}
-
 
 function renderBigImage(title, url) {
   const bigImage = document.querySelector("#popup__image-template").content;
@@ -139,7 +129,7 @@ function renderBigImage(title, url) {
   imageContainer.querySelector(".popup__image").src = url;
   imageContainer.querySelector(".popup__image").alt = title;
   imageContainer.querySelector(".popup__image-text").textContent = title.slice(9);
-  imageContainer.querySelector(".popup__image-close-button").addEventListener("click", function (e) {
+  imageContainer.querySelector(".popup__image-close-button").addEventListener("click", (e) => {
     popup.remove();
     imageContainer.remove();
     e.preventDefault();
@@ -149,24 +139,21 @@ function renderBigImage(title, url) {
   popup.append(imageContainer);
 }
 
-//konami code do something
+//extras 
 let konamiCodePosition = 0;
 const konamicode = ["arrowup", "arrowup", "arrowdown", "arrowdown", "arrowleft", "arrowright", "arrowleft", "arrowright", "a", "b"];
 
 document.addEventListener('keydown', function(e) {
-  
   if (e.key.toLowerCase() === konamicode[konamiCodePosition]) {
-    konamiCodePosition = konamiCodePosition + 1;
-  } else if (e.key.toLowerCase() !== konamicode[konamiCodePosition]) {
-      if (e.key.toLowerCase() === "arrowup") {
-        konamiCodePosition = 1;
-      } else {
-          konamiCodePosition = 0;
-        }
+    konamiCodePosition += 1;
+  } else if (!(e.key.toLowerCase() === konamicode[konamiCodePosition]) && e.key.toLowerCase() === "arrowup") {
+    konamiCodePosition = 1;
+  } else {
+    konamiCodePosition = 0;
   }
 
   if (konamiCodePosition === 10) {
     addInitialCards();
-    alert("Hadoken");
+    alert("Hadóóóken");
   }
 })
